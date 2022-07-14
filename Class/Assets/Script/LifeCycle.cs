1a2aeed338add121922a11d0e7ff0b96f0559aae
1 <101865591+Qyubee99@users.noreply.github.com>
2022-07-14 19:47:12 +0900

    Unity Class day3

--- Class/Assets/Script/LifeCycle.cs
+++ Class/Assets/Script/LifeCycle.cs
@@ -1,13 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Data
+{
+    // 변수
+    // 기본 접근 지정자는 private입니다.
+    public int price;
+    public string name;
+    public Sprite sprite;
+    
+    // 기본 생성자
+    // 생성자를 선언하지 않으면 자동으로 기본 생성자를 생성시켜줍니다.
+
+}
 
 public class LifeCycle : MonoBehaviour
 {
-    int health;
+    // 유니티에서는 소멸자가 존재하지 않습니다.
+    public Data[] data; // 배열의 크기 (?)
+
+    public Text[] priceUI;
+    public Text[] nameUI;
+    public Image[] spriteUI;
+
     private void Awake() // <- 첫번째
-    {
-        Debug.Log("Tiger Awake");
+    { 
+        for(int i = 0; i < data.Length; i++)
+        {
+            // 0 <--------- 0
+            priceUI[i].text = data[i].price.ToString();
+            nameUI[i].text = data[i].name;
+            spriteUI[i].sprite = data[i].sprite;
+        }
     }
     void Start()
     {
@@ -23,13 +50,17 @@ public class LifeCycle : MonoBehaviour
     }
     private void Update()
     {
-        health++;
-        Debug.Log(health);
-
-        if(health >= 100)
-        {
-            health = 0;
-        }
+    
+    }
+    // 게임 오브젝트가 비활성화될 때마다 호출되는 함수
+    private void OnDisable()
+    {
+        Debug.Log("Tiger OnDisable");
+    }
+    // 게임 오브젝트가 파괴되는 순간 호출되는 함수
+    private void OnDestroy()
+    {
+        
     }
 }
 // 초기회 : 게임이 실핼될 때 단 한번만 호출됩니다.

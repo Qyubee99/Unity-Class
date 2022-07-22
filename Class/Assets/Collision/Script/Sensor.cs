4283bf713e053c396703c86485e63eaca9884840
1 <101865591+Qyubee99@users.noreply.github.com>
2022-07-22 19:48:12 +0900

    Unity Class day8
    Collision Class

---
+++ Class/Assets/Collision/Script/Sensor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Sensor : MonoBehaviour
+{
+    private VideoPlayer video;
+
+    void Start()
+    {
+         video = GetComponent<VideoPlayer>();  
+    }
+
+    // 물리적인 충돌을 하지 않고 충돌을 했을 때 동작하는 함수입니다.
+    private void OnTriggerEnter(Collider other)
+    {
+        video.Play(); 
+    }
+
+    // 물리적인 충돌을 하지 않고 충돌을 하고 있을 때 동작하는 함수입니다.
+    private void OnTriggerStay(Collider other)
+    {
+        transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+    }
+
+    // 물리적인 충돌을 하지 않고 충돌을 벗어났을 때 동작하는 함수입니다.
+    private void OnTriggerExit(Collider other)
+    {
+        video.Stop();
+        transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+
+}
